54a75fef8cd6fde0ec750b03e67a32feed8d2f03
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2024-01-05 18:05:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SliderComponent from './SliderComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Slider Example</h1>
+      <SliderComponent />
+    </div>
+  );
+};
+
+export default App;
